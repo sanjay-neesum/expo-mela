42408ebaa40bbd7e903fe6497eeba171f99633ca
sanjay-neesum <sanjay.thakur@neesum.com>
2022-11-13 19:35:27 +0545

    mela webview

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView, ActivityIndicator } from "react-native";
+import { WebView } from "react-native-webview";
+
+const displaySpinner = () => {
+  return (
+    <SafeAreaView style={styles.activityContainer}>
+      <ActivityIndicator size="large" color="#0000ff" />
+    </SafeAreaView>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="dark" barStyle="light-content" />
+      <WebView
+        style={styles.container}
+        bounces={false}
+        startInLoadingState={true}
+        renderLoading={displaySpinner}
+        source={{ uri: "https://devmelaui.gauribaba.com/" }}
+        showsHorizontalScrollIndicator={false}
+        scalesPageToFit
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  activityContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    backgroundColor: "white",
+    height: "100%",
+    width: "100%",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
